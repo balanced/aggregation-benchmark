d313d9e8f8129e10e6dd3326bcb5dc89fa7cd15d
Victor Lin <bornstub@gmail.com>
2014-05-06 13:39:26 -0700

    Init project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+packages = find_packages('.', exclude=('tests', 'tests.*', 'db', 'db.*'))
+
+
+setup(
+    name='benchmark',
+    packages=packages,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'SQLALchemy==0.9.4',
+    ],
+)
